84c3e1d097dcb7e98365553e1fc59032dca3a3aa
Steven Frieson <sfrieson@gmail.com>
2022-06-22 17:11:39 -0400

    Prettifies index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,12 @@
-import CircularDependencyPlugin from 'circular-dependency-plugin'
+import CircularDependencyPlugin from "circular-dependency-plugin";
 
 declare interface VisualizationConfig {
-  filepath: string
+  filepath: string;
 }
 
-declare function circularDependencyVisualizer (pluginConfig: CircularDependencyPlugin.Options, visualizationConfig: VisualizationConfig): CircularDependencyPlugin.Options
+declare function circularDependencyVisualizer(
+  pluginConfig: CircularDependencyPlugin.Options,
+  visualizationConfig: VisualizationConfig
+): CircularDependencyPlugin.Options;
 
 export default circularDependencyVisualizer;
